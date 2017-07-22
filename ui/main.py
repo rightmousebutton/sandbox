3d2a79237f74b78da36c9dbd7f3733049d6b29fd
Maya <rightmousebutton@gmail.com>
2017-07-22 09:26:26 +0900

    Add flake8 test files

---
+++ ui/main.py
@@ -0,0 +1,17 @@
+"""
+PEP8 linting test file
+"""
+
+
+def my_funnc():
+    """
+    My test fucntion
+
+    PEP8 might take a little getting used to
+    """
+
+    print("Hello world!")
+
+    print('what')
+
+    print("why")
